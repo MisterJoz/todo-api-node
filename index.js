1c1bf4d660aed1cfda692f7c50c05e7dc0c1ed7a
JoshGarzagit config --global user.email jegarza98@gmail..com <jegarza98@gmail.com>
2020-01-11 16:50:17 -0600

    Define the Create route

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 let express = require('express'),
     app = express(), 
-    PORT = 3000;
+    PORT = 3000,
+    bodyParser = require('body-parser');
 
+
+// exports of todos.js
+let todoRoutes = require('./routes/todos');
+
+//access the request body 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res, next){
-    res.json({message: "Hi from JS objet"});
-})
+    res.send('Hello from the root route');
+});
+
+app.use('/api/todos', todoRoutes); 
 
 app.listen(PORT, function(){
     console.log('App is running on PORT ' + PORT);
--- routes/todos.js
+++ routes/todos.js
@@ -2,10 +2,21 @@ let express = require('express');
 let router = express.Router();
 let db = require('../models'); // access todo model
 
+
 router.get('/', function(req, res, next){
     db.Todo.find()
     .then(function(todos){
         res.json(todos);
+    })
+    .catch(function(err){
+        res.send(err);
+    });
+});
+
+router.post('/', function(req, res, next){
+    db.Todo.create(req.body)
+    .then(function(newTodo){
+        res.status(201).json(newTodo);
     })
     .catch(function(err){
         res.send(err);
