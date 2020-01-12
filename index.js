3990d4d0bc1c16c6beca9bcf8c6ee2387d7becd5
JoshGarzagit config --global user.email jegarza98@gmail..com <jegarza98@gmail.com>
2020-01-12 17:53:08 -0600

    Serve static html file

--- index.js
+++ index.js
@@ -10,9 +10,11 @@ let todoRoutes = require('./routes/todos');
 //access the request body 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views'));
 
 app.get('/', function(req, res, next){
-    res.send('Hello from the root route');
+    res.sendFile('index.html');
 });
 
 app.use('/api/todos', todoRoutes); 
