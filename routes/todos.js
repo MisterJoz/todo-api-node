70f073373c5620df624847ca702bfae2ed8d6db2
JoshGarzagit config --global user.email jegarza98@gmail..com <jegarza98@gmail.com>
2020-01-11 21:07:02 -0600

    Add show route

--- routes/todos.js
+++ routes/todos.js
@@ -3,6 +3,7 @@ let router = express.Router();
 let db = require('../models'); // access todo model
 
 
+// index route
 router.get('/', function(req, res, next){
     db.Todo.find()
     .then(function(todos){
@@ -13,6 +14,7 @@ router.get('/', function(req, res, next){
     });
 });
 
+// create route, create a todo
 router.post('/', function(req, res, next){
     db.Todo.create(req.body)
     .then(function(newTodo){
@@ -22,6 +24,24 @@ router.post('/', function(req, res, next){
         res.send(err);
     });
 });
+
+// show route, retreive a todo
+router.get('/:todoId', function(req, res, next){
+    //retrieve todo item by id as a param
+    db.Todo.findById(req.params.todoId)
+    .then(function(foundTodo){
+        res.json(foundTodo);
+    })
+    .catch(function(err){
+        res.send(err);
+    });
+});
+
+
+// update route
+
+// delete route
+
 
 // export the router
 module.exports = router;
