304400231b9cc2c37a388829511431daa20bbb1a
JoshGarzagit config --global user.email jegarza98@gmail..com <jegarza98@gmail.com>
2020-01-13 09:04:34 -0600

    Make the delete button work

--- public/app.js
+++ public/app.js
@@ -2,10 +2,19 @@ $(document).ready(function(){
     $.getJSON('/api/todos')
     .then(addTodos)
 
+    // user hits enter
     $('#todoInput').keypress(function(event){
         if(event.which == 13){
             createTodo();
         }
+    });
+
+    // user clicks X
+    // Add the listener to the list because it is avaiable when
+    // the page loads. (Event delegation)
+    // Listening specifcally for clicks on the span.
+    $('.list').on('click', 'span', function(){
+        removeTodo($(this).parent());
     });
 });
 
@@ -18,9 +27,11 @@ function addTodos(todos) {
 
 //add new list item using todo
 function addTodo(todo){
-    let newTodo = $('<li class="task">'+todo.name +'</li>');
+    let newTodo = $('<li class="task">'+todo.name +' <span>X</span></li>');
     if(todo.completed) newTodo.addClass('done');
     $('.list').append(newTodo);
+    //store mongo _id
+    newTodo.data('id', todo._id);
 }
 
 function createTodo(){
@@ -35,4 +46,21 @@ function createTodo(){
     .catch(function(err){
         console.log(err);
     });
+}
+
+function removeTodo(todo){
+    let clickedId = todo.data('id');
+    let deleteUrl = '/api/todos/' + clickedId;
+
+    // delete todo from db
+    $.ajax({
+        method: 'DELETE',
+        url: deleteUrl,
+    })
+    .then(function(data){
+        todo.remove();
+    })
+    .catch(function(err){
+        console.log(err);
+    })
 }
