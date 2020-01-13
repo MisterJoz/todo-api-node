3db76a5acd0b15dc198a9062316d7fa8a3a9485c
JoshGarzagit config --global user.email jegarza98@gmail..com <jegarza98@gmail.com>
2020-01-12 21:13:15 -0600

    Connect front-end form to API

--- public/app.js
+++ public/app.js
@@ -1,14 +1,38 @@
 $(document).ready(function(){
     $.getJSON('/api/todos')
     .then(addTodos)
+
+    $('#todoInput').keypress(function(event){
+        if(event.which == 13){
+            createTodo();
+        }
+    });
 });
 
 function addTodos(todos) {
     // add todos to page here
     todos.forEach(function(todo){
-        // make new list item 
-        let newTodo = $('<li class="task">'+todo.name +'</li>');
-        if(todo.completed) newTodo.addClass('done');
-        $('.list').append(newTodo);
+        addTodo(todo);
+    });
+}
+
+//add new list item using todo
+function addTodo(todo){
+    let newTodo = $('<li class="task">'+todo.name +'</li>');
+    if(todo.completed) newTodo.addClass('done');
+    $('.list').append(newTodo);
+}
+
+function createTodo(){
+    //send request to create new todo
+    let userInput = $('#todoInput').val();
+    $.post('/api/todos', {name: userInput})
+    .then(function(newTodo){
+        $('#todoInput').val('');
+        addTodo(newTodo);
+
+    })
+    .catch(function(err){
+        console.log(err);
     });
 }
